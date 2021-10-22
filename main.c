6708b94bdfbfbb5525c7e62d977e80e243d35543
Alexis Coquelet <acoquele@fu-r2-p1.s19.be>
2021-10-22 14:49:18 +0200

    third commit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: acoquele <acoquele@student@.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 13:53:07 by acoquele          #+#    #+#             */
-/*   Updated: 2021/10/21 11:49:47 by acoquele         ###   ########.fr       */
+/*   Updated: 2021/10/22 14:14:48 by acoquele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,50 +55,48 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	return (NULL);
 }
 
-int ft_strlen_modif(char *a)
-{
-    int i;
 
-    i = 0;
-    while(*a++)
-        i++;
-    printf("%d\n",i);
-    return (i);
+char *get_next_line(int fd)
+{
+	char *a;
+	int buffer;
+
+	
+//	fd = open("README.txt", O_RDONLY);
+	//printf("%d",fd);
+	while((buffer = read(fd,a,1)) > 0)
+	{
+//		printf("%s",a);
+		//printf("%d",buffer);
+		if (ft_memchr(a,'\n',buffer))
+			break;
+	}
+	return (a);
 }
 
-char *get_next_line(void)
-{
-    char a[2];
-    int fd;
-    int buffer;
-
-    buffer = 10;
-    fd = open("README.txt", O_RDONLY);
-    //printf("%d",fd);
-    while((buffer = read(fd,a,1)) > 0)
-    {
-        printf("%s",a);
-        //printf("%d\n",buffer);
-        if (ft_memchr(a,'\n',buffer))
-            break;
-    }
-    return (a);
-}
 int main()
 {
-    int index = 10;
-    while (index-- > 0)
-        get_next_line();        
+	char *s;
+	int fd;
+
+	fd = open("README.txt", O_RDONLY);
+	s = get_next_line(fd);
+	while (s)
+	{
+		printf("%s", s);
+		s = get_next_line(fd);
+	}
+	close(fd);
 }
 
 /*
 read enable you the possibility to read a file descriptor...
 size_t read(int file_descriptor,void *buffer, int nbr_of_byte) 
 read :
-    send 0 if it reach the end of the fonction;
-    send -1 if there is a bug;
+	send 0 if it reach the end of the fonction;
+	send -1 if there is a bug;
 i need to find a way to stop the read at the end of the line
 the pipe of read is stored in the heap memmory
-
+how to use a struct as a static
 
 */
